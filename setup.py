516d0269b8376792f2a84a1a812ea8aae452b8d3
A-Harsha-Vardhan <harshaankem04@gmail.com>
2026-03-14 16:05:58 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="A.Harsha Vardhan",
+    author_email="harshaankem04@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
